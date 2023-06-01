ba0022f2ffc860f52d6ae52b01307afc708df6d4
ohodrax <rodrigo.cassiano00@gmail.com>
2023-06-01 17:48:05 -0300

    Course until class 99

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  // appId: 'io.ionic.starter',
+  addId: 'com.udemy-course.ionic-angular',
   appName: 'ionic-angular-course',
   webDir: 'www',
   server: {
