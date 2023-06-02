630831710c39480c2bed630b269541f867d67f98
ohodrax <rodrigo.cassiano00@gmail.com>
2023-06-02 17:36:30 -0300

    Until class 115

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,10 +7,23 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://imgs.6sqft.com/wp-content/uploads/2014/06/21042534/Felix_Warburg_Mansion_007.jpg',
+      149.99
+    ),
+    new Place(
+      'p2',
+      'L\'Amour Toujours',
+      'A romantic place in Paris.',
+      'https://media-cdn.tripadvisor.com/media/photo-s/16/0b/96/09/hotel-amour.jpg',
+      189.99
+    ),
+    new Place(
+      'p3',
+      'The Foggy Palace',
+      'Not your average city trip!',
+      'https://media.zenfs.com/en/conde_nast_traveler_225/6ba3ac9a3704cfb1d3dd59c53c66227c',
+      99.99
+    ),
+  ];
+
+  get places(){
+    return [...this._places];
+  }
+
+  constructor() { }
+}
