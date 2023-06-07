f9156445421697c339cd1dbfb59d9cc4dacad12c
sparkmaster <rodrigo.cassiano00@gmail.com>
2023-06-06 22:41:09 -0300

    Until class 121

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import { MenuController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -13,13 +13,18 @@ export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
 
   constructor(
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private menuCtrl: MenuController
   ) { }
 
   ngOnInit() {
 
     this.loadedPlaces = this.placesService.places;
 
+  }
+
+  onOpenMenu(){
+    this.menuCtrl.toggle();
   }
 
 }
